5991bc3df594f8af32c503f110d0cb9d9729f80f
Ledroid <87201247+Ledroid@users.noreply.github.com>
2021-07-12 00:59:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const config = require("./config.json")
 
 // Once Ready
 client.on("ready", async () => {
-    console.log(`Oh bot is ready, it can be used now`)
+    console.log(`Oh bot is ready, it can be used now | ${client.guilds.cache.size} Servers | ${client.channels.cache.size} Channels | ${client.users.cache.size} Users`)
 });
 
 // Logging in
